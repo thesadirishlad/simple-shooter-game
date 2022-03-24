5fc1fe7eb8c78895198e18cb1dd5681213adb116
home-lessjesus <102327068+home-lessjesus@users.noreply.github.com>
2022-03-24 22:07:35 +0100

    player

--- main.ts
+++ main.ts
@@ -1,1 +1,20 @@
 scene.setBackgroundColor(1)
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . f f . . . . . 
+    . . . . . . . . f f f . . . . . 
+    . . . . . . . f f . f . . . . . 
+    . . . . . . . f . . f . . . . . 
+    . . . . . . . f . . f . . . . . 
+    . . . . . . . f f f f . . . . . 
+    . . . . . . . f . . f . . . . . 
+    . . . . f f . f . . f f . . . . 
+    . . . f f f f f f f f f f f . . 
+    . . . f f . . f f . . . . f . . 
+    . . f . . . . f f . . . . f . . 
+    . . f . . . . f f . . . f f . . 
+    . . . f f f f f f f f f . . . . 
+    . . . f f . . . . . . . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite)

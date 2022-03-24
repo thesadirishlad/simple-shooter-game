97c0dc6fdad948f045ecc164b80bd6dc61588c43
home-lessjesus <102327068+home-lessjesus@users.noreply.github.com>
2022-03-24 22:38:16 +0100

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

---
+++ images.g.ts
@@ -0,0 +1,55 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "myAnim":
+            case "anim1":return [img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`];
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
--- main.ts
+++ main.ts
@@ -1,20 +1,63 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . f . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, test, -100, 0)
+    music.bigCrash.play()
+})
+let projectile: Sprite = null
+let test: Sprite = null
 scene.setBackgroundColor(1)
-let test = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . f f . . . . . 
-    . . . . . . . . f f f . . . . . 
-    . . . . . . . f f . f . . . . . 
-    . . . . . . . f . . f . . . . . 
-    . . . . . . . f . . f . . . . . 
-    . . . . . . . f f f f . . . . . 
-    . . . . . . . f . . f . . . . . 
-    . . . . f f . f . . f f . . . . 
-    . . . f f f f f f f f f f f . . 
-    . . . f f . . f f . . . . f . . 
-    . . f . . . . f f . . . . f . . 
-    . . f . . . . f f . . . f f . . 
-    . . . f f f f f f f f f . . . . 
-    . . . f f . . . . . . . . . . . 
+test = sprites.create(img`
+    . . . . . . f f . f . . . . . . 
+    . . . . f f f . . f . . . . . . 
+    . . . . f . . . . f f . . . . . 
+    . . . . f . f . f . f f . . . . 
+    . . . . f . . . . . . f . . . . 
+    . . . . . f . . . . f f . . . . 
+    . . . . . . f f f f f . . . . . 
+    . . f f f f . . f . . . . . . . 
+    . . . f . f . . f . . . . . . . 
+    . . . . . f f f f f f f f f f . 
+    . . . . . . . . f . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . . . . f . . f . . . . . . 
+    . . . . . f f . . f . . . . . . 
+    . . . . . . . . . f . . . . . . 
     `, SpriteKind.Player)
+let soldier = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . 7 7 7 7 7 7 7 . . . . 
+    . . . . . 7 7 7 7 7 7 7 . . . . 
+    . . . . . 7 f f f f f 7 . . . . 
+    . . . . . 7 f . . . f 7 . . . . 
+    . . . . . . f . . . f . . . . . 
+    . . . . . . f f f f f . . . . . 
+    . . . . . . . . 7 . . . . . . . 
+    . . . . . . . . 7 . . f f f f . 
+    . . . . f 7 7 7 7 7 f f . f . . 
+    . . . . . . . . 7 . . f . . . . 
+    . . . . . . . . 7 . . . . . . . 
+    . . . . . . . 7 . 7 . . . . . . 
+    . . . . . . 7 . . . 7 . . . . . 
+    . . . . . f . . . . . f . . . . 
+    `, SpriteKind.Enemy)
 controller.moveSprite(test)
+soldier.follow(test, 40)
+info.setLife(3)
